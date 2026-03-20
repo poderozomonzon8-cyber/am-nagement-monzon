b9fe8267f52c81fef8830076930e9c59a3a5b990
poderozomonzon8-cyber <poderozomonzon8@gmail.com>
2026-03-20 14:26:45 -0400

    Initial commit from your app

---
+++ src/index.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { AnimaProvider } from "@animaapp/playground-react-sdk";
+import { ThemeProvider } from "@/contexts/ThemeContext";
+import { SplashProvider } from "@/contexts/SplashContext";
+import { AuthProvider } from "@/contexts/AuthContext";
+import { NotificationProvider } from "@/contexts/NotificationContext";
+import { AnimationProvider } from "@/contexts/AnimationContext";
+import { ThemeAnimationBridge } from "@/components/ThemeAnimationBridge";
+import { SoundProvider } from "@/contexts/SoundContext";
+import NightModeOverlay from "@/components/NightModeOverlay";
+import AmbientSoundWidget from "@/components/AmbientSoundWidget";
+import App from "./App";
+import "./index.css";
+
+/* Register Service Worker for PWA */
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js", { scope: "/" })
+      .catch((err) => console.warn("[PWA] SW registration error:", err));
+  });
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <AnimaProvider>
+      <ThemeProvider>
+        <AnimationProvider>
+          <SplashProvider>
+            <AuthProvider>
+              <NotificationProvider>
+                <SoundProvider>
+                  <ThemeAnimationBridge />
+                  <NightModeOverlay />
+                  <AmbientSoundWidget />
+                  <App />
+                </SoundProvider>
+              </NotificationProvider>
+            </AuthProvider>
+          </SplashProvider>
+        </AnimationProvider>
+      </ThemeProvider>
+    </AnimaProvider>
+  </React.StrictMode>
+);
